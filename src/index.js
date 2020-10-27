f12f59812ff3676a48df9a901b9f286b62a4d693
Alex Hoyer <alexhoyer@Alexs-MBP.home>
2020-10-27 06:30:38 -0700

    added error handling to app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App'
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>,
+  ,
   document.getElementById('root')
 );
 
