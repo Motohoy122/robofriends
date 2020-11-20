e5e26cf82636995690170f4d8fbd7eb7d502aa49
Alex Hoyer <alexhoyer@Alexs-MBP.home>
2020-11-19 23:25:09 -0800

    added react hooks to robofriends app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,29 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-const state = {
+
+function App() {
   
-}
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: ''
-    }
-  }
+  const [robots, setRobots] = useState([]);
+  const [searchfield, setSearchfield] = useState('');
+  const [count, setCount] = useState(0);
 
-  componentDidMount() {
+  useEffect(()=> {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-    .then(users => this.setState({robots: users}));
-    
-  }
+    .then(response => response.json())
+    .then(users => {setRobots(users)});
+    console.log(count)
+  }, [count])
 
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value})
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
     
     
   }
-  render() {
-    const {robots, searchfield} = this.state;
     const filterRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
@@ -40,7 +33,8 @@ class App extends Component {
     (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <button onClick={()=>setCount(count + 1)}>Click Me!</button>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <ErrorBoundary>
               <CardList robots={filterRobots}/>
@@ -50,6 +44,6 @@ class App extends Component {
     );
     
   }
-}
+
 
 export default App;
