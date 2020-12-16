ae0c9676243901d5422a892689c7732350f97287
Alex Hoyer <alexhoyer@Alexs-MBP.home>
2020-12-15 21:29:00 -0800

    Added Redux functionality

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {setSearchField, requestRobots} from '../actions';
+
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -6,34 +9,43 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 
-function App() {
-  
-  const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState('');
-  const [count, setCount] = useState(0);
 
-  useEffect(()=> {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => {setRobots(users)});
-    console.log(count)
-  }, [count])
-
-  const onSearchChange = (event) => {
-    setSearchfield(event.target.value)
-    
-    
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending
   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
+
+class App extends Component {
+
+
+componentDidMount() {
+  this.props.onRequestRobots();
+
+}
+
+ 
+
+render() {
+    const {searchField, onSearchChange, robots, isPending} = this.props;
     const filterRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
 
-    return !robots.length ? 
+    return isPending ? 
     <h1>Loading</h1> :
     (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <button onClick={()=>setCount(count + 1)}>Click Me!</button>
           <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <ErrorBoundary>
@@ -44,6 +56,6 @@ function App() {
     );
     
   }
+}
 
-
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import {Provider} from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk'
 import './index.css';
 import App from './containers/App'
 import reportWebVitals from './reportWebVitals';
+import {searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
+const logger = createLogger();
+
+const rootReducers = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
-  
-    <App />
+    // <Provider store={store}>
+      <App store={store}/>
+    // </Provider>
   ,
   document.getElementById('root')
 );
